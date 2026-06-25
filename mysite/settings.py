e28f2b4568f94b9aa0609498429e7eeba6420ea8
unknown <b11213032@yuntech.edu.tw>
2026-06-25 22:43:32 +0800

    更改內容3

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'NAME': 'smart_pot_db',         # 你們大專案的資料庫名稱
         'USER': 'group5_admin',                 # MySQL 帳號
         'PASSWORD': 'admin_password_555',     # 你們設定的 MySQL 密碼
-        'HOST': '127.0.0.1',        # 🌟 這裡必須填你們 MySQL 在 docker-compose 裡的 service 名稱！
+        'HOST': 'db',        # 🌟 這裡必須填你們 MySQL 在 docker-compose 裡的 service 名稱！
         'PORT': '3306',
     }
 }
