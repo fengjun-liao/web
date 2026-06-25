5ecbdf005e53f9aec0170eaa8544af12f426bb2b
unknown <b11213032@yuntech.edu.tw>
2026-06-26 00:35:44 +0800

    更改內容12

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,12 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/member2/', views.index, name='index'),
-    path('/member2/about/', views.about, name='about'),
-    path('/member2/dashboard/', views.sensor_dashboard, name='dashboard'),
-    path('/member2/sensor/<int:sensor_id>/', views.sensor_detail, name='sensor_detail'),
-    path('/member2/api/sensor/<int:sensor_id>/data/', views.sensor_data_api, name='sensor_data_api'),
-    path('/member2/api/chart-history/', views.chart_history_api, name='chart_history_api'),
-    path('/member2/raw-data/', views.raw_data_view, name='raw_data'),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('dashboard/', views.sensor_dashboard, name='dashboard'),
+    path('sensor/<int:sensor_id>/', views.sensor_detail, name='sensor_detail'),
+    path('api/sensor/<int:sensor_id>/data/', views.sensor_data_api, name='sensor_data_api'),
+    path('api/chart-history/', views.chart_history_api, name='chart_history_api'),
+    path('raw-data/', views.raw_data_view, name='raw_data'),
     path('projects/', views.projects, name='projects'),
 ]
